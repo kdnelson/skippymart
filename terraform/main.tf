31cd8ef7b2a819b32f06bbc516269aa5be1a1be9
Kris Nelson <kris.d.nelson@gmail.com>
2026-09-17 13:44:26 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,43 +15,41 @@ resource "aws_s3_bucket_public_access_block" "skippymart" {
   restrict_public_buckets = true
 }
 
-resource "aws_acm_certificate" "skippymart_cert" {
-  domain_name       = "skippymart.com"
-  validation_method = "DNS"
-  subject_alternative_names = [ "www.skippymart.com" ]
+# resource "aws_acm_certificate" "skippymart_cert" {
+#   domain_name       = "skippymart.com"
+#   validation_method = "DNS"
+#   subject_alternative_names = [ "www.skippymart.com" ]
 
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
 
-data "aws_route53_zone" "domain_zone" {
-  name         = "skippymart.com"
-  private_zone = false
-}
+# data "aws_route53_zone" "domain_zone" {
+#   name         = "skippymart.com"
+#   private_zone = false
+# }
 
-resource "aws_route53_record" "skippymart_cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.skippymart_cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      type   = dvo.resource_record_type
-      record = dvo.resource_record_value
-    }
-  }
+# resource "aws_route53_record" "skippymart_cert_validation" {
+#   for_each = {
+#     for dvo in aws_acm_certificate.skippymart_cert.domain_validation_options : dvo.domain_name => {
+#       name   = dvo.resource_record_name
+#       type   = dvo.resource_record_type
+#       record = dvo.resource_record_value
+#     }
+#   }
 
-  zone_id = data.aws_route53_zone.domain_zone.id
-  name    = "www.${data.aws_route53_zone.domain_zone.name}"
-  type    = each.value.type
-  ttl     = 60
-  records = [each.value.record]
-  allow_overwrite = true #  May already exist: [name='www.skippymart.com.', type='CNAME']
-}
+#   zone_id = data.aws_route53_zone.domain_zone.id
+#   name    = "www.${data.aws_route53_zone.domain_zone.name}"
+#   type    = each.value.type
+#   ttl     = 60
+#   records = [each.value.record]
+#   allow_overwrite = true
+# }
 
-resource "aws_acm_certificate_validation" "skippymart_cert_validation" {
-  certificate_arn         = aws_acm_certificate.skippymart_cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.skippymart_cert_validation : record.fqdn]
-
-  timeouts {
-    create = "2h" #  Terraform will poll for up to 2 hours to validate the certificate.
-  }
-}
+# Assuming we have a valid ACM certificate, we validate it using the DNS records above. 
+# This resource will handle the validation process.
+# resource "aws_acm_certificate_validation" "skippymart_cert_validation" {
+#   certificate_arn         = aws_acm_certificate.skippymart_cert.arn
+#   validation_record_fqdns = [for record in aws_route53_record.skippymart_cert_validation : record.fqdn]
+# }
