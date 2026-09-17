e3ecf4144ecb799f4b1145200fb1a7493a650861
Kris Nelson <kris.d.nelson@gmail.com>
2026-09-17 14:23:04 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,31 +26,32 @@ resource "aws_acm_certificate" "skippymart_cert" {
   }
 }
 
-# data "aws_route53_zone" "domain_zone" {
-#   name         = "skippymart.com"
-#   private_zone = false
-# }
+data "aws_route53_zone" "domain_zone" {
+  name         = "skippymart.com"
+  private_zone = false
+}
 
-# resource "aws_route53_record" "skippymart_cert_validation" {
-#   for_each = {
-#     for dvo in aws_acm_certificate.skippymart_cert.domain_validation_options : dvo.domain_name => {
-#       name   = dvo.resource_record_name
-#       type   = dvo.resource_record_type
-#       record = dvo.resource_record_value
-#     }
-#   }
+resource "aws_route53_record" "skippymart_cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.skippymart_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
 
-#   zone_id = data.aws_route53_zone.domain_zone.id
-#   name    = "www.${data.aws_route53_zone.domain_zone.name}"
-#   type    = each.value.type
-#   ttl     = 60
-#   records = [each.value.record]
-#   allow_overwrite = true
-# }
+  zone_id = data.aws_route53_zone.domain_zone.id
+  name    = each.value.name
+  # name    = "www.${data.aws_route53_zone.domain_zone.name}"
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.record]
+  allow_overwrite = true
+}
 
 # Assuming we have a valid ACM certificate, we validate it using the DNS records above. 
 # This resource will handle the validation process.
-# resource "aws_acm_certificate_validation" "skippymart_cert_validation" {
-#   certificate_arn         = aws_acm_certificate.skippymart_cert.arn
-#   validation_record_fqdns = [for record in aws_route53_record.skippymart_cert_validation : record.fqdn]
-# }
+resource "aws_acm_certificate_validation" "skippymart_cert_validation" {
+  certificate_arn         = aws_acm_certificate.skippymart_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.skippymart_cert_validation : record.fqdn]
+}
