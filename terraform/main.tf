0533d2b1cf87e0cab59c1db19b023c0070bda3d8
Kris Nelson <kris.d.nelson@gmail.com>
2026-09-16 19:31:08 -0700

    Adding Route53

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,3 +24,30 @@ resource "aws_acm_certificate" "skippymart_cert" {
     create_before_destroy = true
   }
 }
+
+data "aws_route53_record" "domain_zone" {
+  zone_id = var.route53_zone_id
+  name    = "skippymart.com"
+  type    = "A"
+}
+
+resource "aws_route53_record" "skippymart_cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.skippymart_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  zone_id = data.aws_route53_record.domain_zone.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.record]
+}
+
+resource "aws_acm_certificate_validation" "skippymart_cert_validation" {
+  certificate_arn         = aws_acm_certificate.skippymart_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.skippymart_cert_validation : record.fqdn]
+}
