c65e6ba8be77c101abcfb971cc0e1b18c0d94cf2
Kris Nelson <kris.d.nelson@gmail.com>
2026-09-18 00:27:48 -0700

    Adding alias A records

--- terraform/main.tf
+++ terraform/main.tf
@@ -153,3 +153,27 @@ resource "aws_cloudfront_distribution" "skippymart_distribution" {
     }
   }
 }
+
+resource "aws_route53_record" "apex" {
+  zone_id = aws_route53_zone.domain_zone.zone_id
+  name    = var.website_name
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.skippymart_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.skippymart_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.domain_zone.zone_id
+  name    = "www.${var.website_name}"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.skippymart_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.skippymart_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
